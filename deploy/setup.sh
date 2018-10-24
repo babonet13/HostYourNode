d0be296aa68d61f4dea607b8ee71cc1837982a29
babonet13 <admin@hubject.net>
2018-10-24 18:08:13 +0200

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,5 +3,5 @@ sudo apt-get install docker.io
 sudo adduser satoshi docker
 docker volume create portainer_data
 docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer-node portainer/portainer
-sudo git clone https://github.com/babonet13/HostYourNode
-sudo chown -R satoshi HostYourNode
+sudo git clone https://github.com/babonet13/HostYourNode /HostYourNode
+sudo chown -R satoshi /HostYourNode
