296f1f6faf05a557c16c940b4f570ee530b1b2f6
babonet13 <admin@hubject.net>
2018-10-25 17:28:42 +0200

    Update setup.sh

--- deploy/ubuntu/setup.sh
+++ deploy/ubuntu/setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get install docker.io
 sudo adduser satoshi docker
 docker volume create portainer_data
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer-node portainer/portainer restart=always
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer-node portainer/portainer
 # Pull the start image
 docker pull ubuntu
 # Clone the repository containing required "recipes"
