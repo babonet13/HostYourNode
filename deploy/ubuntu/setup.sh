008d91679eec74d30dd52350cb61336319b10dfc
babonet13 <admin@hubject.net>
2018-10-29 15:06:11 +0100

    Update setup.sh

--- deploy/ubuntu/setup.sh
+++ deploy/ubuntu/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# Install Docker and Portainer
-sudo apt-get install docker.io
-sudo adduser satoshi docker
+# Install prerequisite
+sudo apt-get install -y curl
+
+# Install Docker
+sudo add-apt-repository \ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"
+sudo apt-get update
+sudo usermod -aG docker satoshi
+
+# Deploying Portainer
 docker volume create portainer_data
 docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer-node portainer/portainer
-# Pull the start image
-docker pull ubuntu
-# Clone the repository containing required "recipes"
-sudo git clone https://github.com/babonet13/HostYourNode /HostYourNode
-sudo chown -R satoshi /HostYourNode
-# Build required images for he Bitcoin stack
-docker build -t ubuntu-pkg /HostYourNode/Docker/ubuntu-pkg
-docker build -t bitcoind-pkg-ubuntu /HostYourNode/Docker/bitcoind-pkg-ubuntu
-# Launch containers based on those images
-docker run -d -v /var/lib/bitcoin:/var/lib/bitcoin -p 8333:8333 --name bitcoin-node bitcoind-pkg-ubuntu
+
+# Deploying the nodes : bitcoind
+docker pull hostyournode/bitcoind_pkg-ubuntu
+docker run -d -v /var/lib/bitcoin:/var/lib/bitcoin -p 8333:8333 --name bitcoin-node hostyournode/bitcoind_pkg-ubuntu
