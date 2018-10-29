f0b85770052d1e2992bcea85aa552652adab437e
babonet13 <admin@hubject.net>
2018-10-29 16:18:39 +0100

    Update setup.sh

--- deploy/ubuntu/setup.sh
+++ deploy/ubuntu/setup.sh
@@ -12,5 +12,7 @@ docker volume create portainer_data
 docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer-node portainer/portainer
 
 # Deploying the nodes : bitcoind
+mkdir /var/lib/bitcoin
+sudo chown -R satoshi:satoshi /var/lib/bitcoin
 docker pull hostyournode/bitcoind_pkg-ubuntu
 docker run -d -v /var/lib/bitcoin:/var/lib/bitcoin -p 8333:8333 --name bitcoin-node hostyournode/bitcoind_pkg-ubuntu
