6e4152b7f85578efa1486b5014ffaa9bb10dc24f
babonet13 <admin@hubject.net>
2018-10-29 17:29:01 +0100

    Update setup.sh

--- deploy/ubuntu/setup.sh
+++ deploy/ubuntu/setup.sh
@@ -2,6 +2,7 @@
 
 # Install Docker
 sudo add-apt-repository \ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"
+sudo apt-get install -y docker-ce
 sudo apt-get update
 sudo usermod -aG docker satoshi
 
